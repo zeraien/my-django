6a86554e416991f0a0571a15e3ce18c5ae9f03ed
Dmitri Fedortchenko <d@angelhill.net>
2011-04-03 14:44:18 +0200

    minor fix in rendering functions

--- mydjango/http/rendering.py
+++ mydjango/http/rendering.py
@@ -5,6 +5,7 @@ from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from django.http import HttpResponseRedirect, HttpResponse, HttpResponseBadRequest, HttpResponsePermanentRedirect
 from django.core.xheaders import populate_xheaders
+from django.utils.decorators import available_attrs
 
 from functools import wraps
 
@@ -12,8 +13,9 @@ from functools import wraps
 def render(request, template, context = {}, ignore_ajax = False, obj=None, **render_kwargs):
     if request.is_ajax() and not ignore_ajax:
         basename = os.path.basename(template)
-        dirname = os.path.dirname(template)
-        template = "%s/_%s"%(dirname,basename)
+        if not basename.startswith("_"):
+            dirname = os.path.dirname(template)
+            template = "%s/_%s"%(dirname,basename)
         response = render_to_response(template, context)
     else:
         response = render_to_response(template, context, context_instance=RequestContext(request), **render_kwargs)
@@ -22,7 +24,7 @@ def render(request, template, context = {}, ignore_ajax = False, obj=None, **ren
     return response
     
 def permanent_redirect(view_func):
-    @wraps(view_func)
+    @wraps(view_func, assigned=available_attrs(view_func))
     def wrapper(request, *args, **kw):
         to = view_func(request, *args, **kw)
         if type(to) in (str, unicode):
@@ -32,7 +34,7 @@ def permanent_redirect(view_func):
     return wrapper
     
 def redirect(view_func):
-    @wraps(view_func)
+    @wraps(view_func, assigned=available_attrs(view_func))
     def wrapper(request, *args, **kw):
         to = view_func(request, *args, **kw)
         if type(to) in (str, unicode):
@@ -42,7 +44,7 @@ def redirect(view_func):
     return wrapper
 
 def render_json(view_func):
-    @wraps(view_func)
+    @wraps(view_func, assigned=available_attrs(view_func))
     def wrapper(request, *args, **kw):
         _json = view_func(request, *args, **kw)
         if not isinstance(_json, str) and not isinstance(_json, dict) and not isinstance(_json, list) and not isinstance(_json, tuple):
@@ -51,14 +53,14 @@ def render_json(view_func):
         return HttpResponse(json.dumps(_json), mimetype=mimetype)
     return wrapper
 
-def render_to(template_name):
+def render_to(template_name, ignore_ajax=False):
     def renderer(func):
-        @wraps(func)
+        @wraps(func, assigned=available_attrs(func))
         def wrapper(request, *args, **kw):
             output = func(request, *args, **kw)
             if not isinstance(output, dict):
                 return output
             output['request'] = request
-            return render(request, template=template_name, context=output)
+            return render(request, template=template_name, context=output, ignore_ajax=ignore_ajax)
         return wrapper
     return renderer
