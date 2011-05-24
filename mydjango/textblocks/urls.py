22614fd1c858abbe0cda4d27a4842a50032087cc
Dmitri Fedortchenko <d@angelhill.net>
2011-05-24 22:19:17 +0200

    added drop and save urlconfs

--- mydjango/textblocks/urls.py
+++ mydjango/textblocks/urls.py
@@ -4,10 +4,10 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('mydjango.textblocks.views',
 	url(r'^(?P<textblock_id>\d+)/$','display',name="textblock_display"),
-	url(r'^save/$','save',name="textblock_save"),
+	url(r'^save/$','save', name="textblock_save"),
 	url(r'^(?P<textblock_id>\d+)/delete/$','delete',name="textblock_delete"),
 	url(r'^(?P<textblock_id>\d+)/edit/$','edit',name="textblock_edit"),
-	url(r'^drop/$','drop',name="textblock_drop"),
+	url(r'^drop/$', 'drop', name="textblock_drop"),
 	url(r'^reset_cache/$','reset_cache',name="textblock_reset_cache"),
 )
 
