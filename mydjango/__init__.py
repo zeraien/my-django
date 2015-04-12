1df1b4b6221f67bc7959f57529977a5b0ee33101
Dmitri Fedortchenko <d@angelhill.net>
2015-04-12 21:13:25 +0200

    version number fix

--- mydjango/__init__.py
+++ mydjango/__init__.py
@@ -1,7 +1,2 @@
 VERSION = (0,2,1)
-# Dynamically calculate the version based on VERSION tuple
-if len(VERSION)>2 and VERSION[2] is not None:
-    __version__ = "%d.%d_%s" % VERSION[:3]
-else:
-    __version__ = "%d.%d" % VERSION[:2]
-
+__version__ = '.'.join(VERSION)
